297275f22010c7ece233c789fa0190df9e57baeb
Unkei Kou <unkei.kou.chs@gmail.com>
2020-12-15 10:51:44 -0500

    Add new columns to Venue and Artist model
    Add website, seeking_talent and seeking_description
    columns to Venue model.
    Add website, seeking_venue and seeking_description
    columns to Artist model.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -41,7 +41,10 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     genres = db.Column(db.ARRAY(db.String(120)))
     image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(200))
     shows = db.relationship('Show', backref='venue', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
@@ -54,9 +57,12 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)))
     image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(200))
     shows = db.relationship('Show', backref='artist', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
