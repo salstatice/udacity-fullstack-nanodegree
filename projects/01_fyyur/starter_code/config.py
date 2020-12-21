4747a8edcf7cd87b0339ea48ca1cd77ba916c822
Unkei Kou <unkei.kou.chs@gmail.com>
2020-12-21 17:28:44 -0500

    Add another way to setup env variables

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -18,4 +18,10 @@ DEBUG = True
 # database = ''
 # SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}:{port}/{database}'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/newfyyur'
+# Another way to handle environment variables
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'your_db')
+
+SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
